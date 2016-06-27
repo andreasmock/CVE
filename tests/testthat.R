f152e288abcce6314cce364e3026d984b5848145
Andreas Mock <mock.science@gmail.com>
2016-06-27 18:17:50 +0200

    debugging of testing

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,9 +7,4 @@ test_that("oncotator example file is in the right format",
             expect_that(oncotator_example, is.data.frame)
           })
 
-test_that("ping local server for Shiny app (less than 1 sec)",
-          {
-          expect_less_than(ping("127.0.0.1",count = 1), 1)
-          })
-
 
